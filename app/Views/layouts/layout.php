12396f7cb735ae19b011ab7969ddf5398dcf91df
siti nurhayati <sitinurhayatiiiiiiii102@gmail.com>
2022-04-18 22:44:48 +0700

    hide menu

--- app/Views/layouts/layout.php
+++ app/Views/layouts/layout.php
@@ -23,12 +23,6 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?= base_url() . "/mahasiswa" ?>">Mahasiswa</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?= base_url() . "/matakuliah" ?>">Matakuliah</a>
-                    </li>
                 </ul>
             </div>
         </div>
